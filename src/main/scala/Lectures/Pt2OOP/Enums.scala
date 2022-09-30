d30669e2e69372f0bca31466fd4adc2e6796f9d4
walter <mywalter97@gmail.com>
2022-09-29 20:32:07 -0600

    Enums

---
+++ src/main/scala/Lectures/Pt2OOP/Enums.scala
@@ -0,0 +1,51 @@
+package Lectures.Pt2OOP
+
+object Enums extends App {
+  println("Enums lecture")
+  // Scala 3 adds support for enums
+  //  But I created the project with scala 2, so all code is commented
+
+  /*
+  enum Permissions {
+    case READ, WRITE
+    , EXECUTE
+
+    // Since this is a data type, we can add fields or methods
+    def openDocument(): Unit =
+      if (this == READ) println("opening document...")
+      else println("Reading not allowed")
+
+  }
+
+  //  we can also define enums with constructor args
+  enum PermissionsWithBits (bits: Int) {
+    case READ extends PermissionsWithBits(4) //100
+    case WRITE
+    extends PermissionsWithBits(2) //010
+    case EXECUTE
+    extends PermissionsWithBits(1) //001
+  }
+
+  //  we can also create companion objects for enums
+  object PermissionsWithBits {
+    def fromBits(bits: Int): PermissionsWithBits = //whatever
+      PermissionsWithBits.READ
+  }
+
+
+  val somePermissions: Permissions = Permissions.READ
+
+  println(somePermissions)
+  somePermissions.openDocument()
+
+  // Standard API
+  // Get the ordinal position of enum value
+  println(somePermissions.ordinal)
+  val allPermissions = PermissionsWithBits.values //array of all possible values
+  println(allPermissions)
+
+  //  create from string
+  val readPermission: Permissions = Permissions.valueOf("READ") //Permissions.RAED
+  println(readPermission)
+   */
+}
