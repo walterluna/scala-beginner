5b0407151be87f8a7c50acc9abd3a0d1e27ed2f8
walter <mywalter97@gmail.com>
2022-10-05 23:18:15 -0600

    Hofs and Curries: Exercises

--- src/main/scala/Exercises/MyList.scala
+++ src/main/scala/Exercises/MyList.scala
@@ -1,6 +1,9 @@
 package Exercises
 
-import scala.annotation.targetName
+import Lectures.Pt2OOP.Generics.MyList
+
+import scala.annotation.{tailrec, targetName}
+import scala.runtime.Nothing$
 
 abstract class MyList[+A] {
 
@@ -32,6 +35,15 @@ abstract class MyList[+A] {
   def filter(predicate: A => Boolean): MyList[A]
 
   def ++[B >: A](list: MyList[B]): MyList[B]
+
+  //  HOFs
+  def forEach(f: A => Unit): Unit
+
+  def sort(f: (A, A) => Int): MyList[A]
+
+  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C]
+
+  def fold[B](acc: B)(folder: (A, B) => B): B
 }
 
 
@@ -55,6 +67,18 @@ case object Empty extends MyList[Nothing] {
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+
+  //  HOFs
+  def forEach(f: Nothing => Unit): Unit = ()
+  //  () is equivalent to Unit
+
+  override def sort(f: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+
+  override def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] =
+    if (!list.isEmpty) throw new RuntimeException("Lists are not same sized")
+    else Empty
+
+  override def fold[B](acc: B)(folder: (Nothing, B) => B): B = acc
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -84,6 +108,33 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
   def ++[B >: A](list: MyList[B]): MyList[B] = Cons(h, t ++ list)
 
+  //  HOFs
+  override def forEach(f: A => Unit): Unit =
+    f(head)
+    tail.forEach(f)
+
+  //  Implementation of bubble sort (Mine)
+  override def sort(compare: (A, A) => Int): MyList[A] =
+    if (tail.isEmpty) Cons(head, Empty)
+    else if (compare(head, tail.head) > 0) Cons(tail.head, Cons(head, tail.tail).sort(compare)).sort(compare)
+    else Cons(head, tail)
+
+  //  Implementation of insert sort (from video) (ex: try making it tailRec)
+  // override def sort(compare: (A, A) => Int): MyList[A] = {
+  //   def insert(x: A, sortedList: MyList[A]): MyList[A] =
+  //     if (sortedList.isEmpty) Cons(x, Empty)
+  //     else if (compare(x, sortedList.head) <= 0) Cons(x, sortedList)
+  //     else Cons(sortedList.head, insert(x, sortedList.tail))
+  //   val sortedTail = tail.sort(compare)
+  //   insert(h, sortedTail)
+  // }
+
+  override def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] =
+    if (list.isEmpty) throw new RuntimeException("Lists are not same sized")
+    else Cons(zip(head, list.head), tail.zipWith(list.tail, zip))
+
+  override def fold[B](acc: B)(folder: (A, B) => B): B =
+    tail.fold(folder(head, acc))(folder)
 }
 
 object ListTest extends App {
@@ -94,9 +145,9 @@ object ListTest extends App {
   //
   //  println(list.toString)
 
-  val listOfIntegers: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+  val listOfIntegers: MyList[Int] = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Empty)))))
   val listOfIntegersClone: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
-  val anotherListOfIntegers: MyList[Int] = Cons(1, Cons(4, Cons(5, Empty)))
+  val anotherListOfIntegers: MyList[Int] = Cons(4, Cons(5, Empty))
   val listOfStrings: MyList[String] = Cons("hello", Cons("scala", Empty))
 
   println(listOfIntegers.toString)
@@ -110,4 +161,13 @@ object ListTest extends App {
   println(listOfIntegers.flatMap((elem) => Cons[Int](elem, Cons[Int](elem + 1, Empty))))
 
   println(listOfIntegers == listOfIntegersClone)
+
+  println("HOF and Curries exercises")
+  listOfIntegers.forEach(println)
+  println(listOfIntegers.sort((x, y) => y - x).toString)
+  println(listOfStrings.zipWith[Int, String](anotherListOfIntegers, _ + "-" + _))
+  val foldOnZero = listOfIntegersClone.fold(0)
+  val foldOnTen = listOfIntegersClone.fold(10)
+  println(foldOnZero(_ + _))
+  println(foldOnTen(_ + _))
 }
--- src/main/scala/Lectures/Pt3FP/HOFsAndCurries.scala
+++ src/main/scala/Lectures/Pt3FP/HOFsAndCurries.scala
@@ -1,5 +1,7 @@
 package Lectures.Pt3FP
 
+import scala.annotation.tailrec
+
 object HOFsAndCurries extends App {
   val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
   // higher order function (HOF)
@@ -10,6 +12,7 @@ object HOFsAndCurries extends App {
   // nTimes(f, n, x)
   // nTimes(f, 3, x) = f(f(f(x))) = nTimes(f, 2, f(x)) = f(f(f(x)))
   // nTimes(f, n, x) = f(f(...f(x))) = nTimes(f, n-1, f(x))
+  @tailrec
   def nTimes(f: Int => Int, n: Int, x: Int): Int =
     if (n <= 0) x
     else nTimes(f, n - 1, f(x))
@@ -28,7 +31,8 @@ object HOFsAndCurries extends App {
   println(plus10(1))
 
   // curried functions
-  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  def superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+
   val add3 = superAdder(3) // y => 3 + y
   println(add3(10))
   println(superAdder(3)(10))
@@ -42,4 +46,43 @@ object HOFsAndCurries extends App {
   println(standardFormat(Math.PI))
   println(preciseFormat(Math.PI))
 
+  /* EXERCISES
+    1. Extend MyList
+      -forEach method A => Unit
+        [1,2,c].forEach(x => println()) => Unit
+      -sort method ((A,A) => Int ) => MyList
+        [1,2,c].sort((x,y) => y-x) => [3,2,1]
+      -zipWith (list, (A, A) => B) => MyList[B]
+        [1,2,c].zipWith([4,5,6], x * y) => [1*4, 2*5, 3*6] = [4,10,18]
+      -fold (curried)
+        [1,2,3].fold(0)(x+y) => 0 + 1 + 2 + 3 = 6
+
+    2. toCurry(f: (Int, Int) => Int) => (Int => Int => Int)
+       fromCurry(Int => Int => Int) => ((Int, Int) => Int)
+
+    3. compose(f, g) => x =>  f(g(x))
+       andThen(f,g) => x => g(f(x))
+  */
+
+  def toCurry[A, B, C](f: (A, B) => C): (A => B => C) =
+    x => y => f(x, y)
+
+  def fromCurry[A, B, C](f: (A => B => C)): (A, B) => C =
+    (x, y) => f(x)(y)
+
+  def compose[A, B, C](f: A => B, g: C => A): C => B =
+    x => f(g(x))
+
+  def andThen[A, B, C](f: A => B, g: B => C): A => C =
+    x => g(f(x))
+
+  def supperAdder2: Int => Int => Int = toCurry(_ + _)
+
+  def add4 = supperAdder2(4)
+
+  val simpleAdder = fromCurry(superAdder)
+
+
+  println(add4(17))
+  println(simpleAdder(4, 17))
 }
